46dc6a65a1fdf2b2c7702338be1d20599decaece
eatnows <gagyasindev@gmail.com>
2020-12-17 01:47:27 +0900

    프로젝트 생성

---
+++ src/main/java/me/eatnows/newcruit/NewcruitApplication.java
@@ -0,0 +1,13 @@
+package me.eatnows.newcruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewcruitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/eatnows/newcruit/NewcruitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.eatnows.newcruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
