634a7ae491e9de660af9b61fc09c037e5f3d6b03
eatnows <gagyasindev@gmail.com>
2020-12-24 22:54:39 +0900

    feat: BaseTimeEntity 설정 #4
    DB에 자동으로 생성시간과 업데이트 시간이 들어가도록 Entity 설정

---
+++ src/main/java/me/eatnows/newcruit/domain/UpdateTimeEntity.java
@@ -0,0 +1,25 @@
+package me.eatnows.newcruit.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class UpdateTimeEntity {
+
+    @CreatedDate
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+}
